74359cddee023fa77c9792282d745acaf29340f9
Sergej Kembel <37798350+SergejKembel@users.noreply.github.com>
2022-05-20 00:47:36 +0000

    remove unnecessary gproj parameter

--- launch.py
+++ launch.py
@@ -112,6 +112,6 @@ else:
 
     config_path = CONFIG_GENERATED
 
-launch = f"{os.environ['ARMA_BINARY']} -gproj ./addons/data/ArmaReforger.gproj -config {config_path} -backendlog -nothrow -profile {os.environ['ARMA_PROFILE']} {os.environ['ARMA_PARAMS']}"
+launch = f"{os.environ['ARMA_BINARY']} -config {config_path} -backendlog -nothrow -profile {os.environ['ARMA_PROFILE']} {os.environ['ARMA_PARAMS']}"
 print(launch, flush=True)
 os.system(launch)
